c2692bac57b90a67f179f060e6a0484e1eacea47
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-05-20 22:35:53 +0500

    Product of vlaues

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('hello')
+# PRODUCT 
+num1 = int(input('Enter first Number  '))
+num2 = int(input('Enter second Number  '))
+product = num1 * num2
+print(f'Product of {num1} and {num2} is : {product}')
+
