41db0908db2ef26c0bc2f2740ca9ad16ad763111
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-05-20 22:41:14 +0500

    Arithmetic operations in python

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+print('Product of values')
 # PRODUCT 
 num1 = int(input('Enter first Number  '))
 num2 = int(input('Enter second Number  '))
 product = num1 * num2
 print(f'Product of {num1} and {num2} is : {product}')
 
+print('Sum of values')
+# ADDITION
+num1 = int(input('Enter first Number  '))
+num2 = int(input('Enter second Number  '))
+sum = num1 +  num2
+print(f'Sum of {num1} and {num2} is : {sum}')
+
+print('Difference of values')
+# SUBTRACTION
+num1 = int(input('Enter first Number  '))
+num2 = int(input('Enter second Number  '))
+sub = num1  -  num2
+print(f'Difference of {num1} and {num2} is : {sub}')
+
+
+print('Division of values')
+# DIVISION
+num1 = int(input('Enter first Number  '))
+num2 = int(input('Enter second Number  '))
+div = num1 /  num2
+print(f'Division of {num1} and {num2} is : {div}')
