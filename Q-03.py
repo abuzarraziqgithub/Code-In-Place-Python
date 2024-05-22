75e1fabc588ff55089b88747616792141cc485bb
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-05-22 20:41:45 +0500

    Q-03-Solved

--- Q-03.py
+++ Q-03.py
@@ -71,14 +71,14 @@
 # print("x and y are equal!")
 
 
-#* MILESTONE 01 PROBLEM :  MARS WEIGHT
+#? MILESTONE 01 PROBLEM :  MARS WEIGHT
 
 # TAKE AN INPUT FROM THE USER TO ENTER WEIGHT.
 # CONVERT THE WEIGHT ON EARTH INTO WEIGHT ON MARS.
 # THE WEIGHT OF MARS IS 37.8% OF WEIGHT ON EARTH
 # ROUND THE RESULT INTO 2 DECIMAL PLACES.
 
-#? PSUEDO CODE :
+#* PSUEDO CODE :
 
 # USER INPUT = FLOAT('ENTER A WEIGHT ON EARTH') 20
 # WEIGHT ON MARS = 37.8%
@@ -86,7 +86,9 @@
 # CALCULATED VALUE = ROUND(USER WEIGHT * 0.378 , 2 )
 # PRINT('THE EQUIVALENT WEIGHT ON MARS:' + CALCULATED VALUE)
 
-#* CODE
+#* CODE :
+
+print('Milestone : 01')
 
 Mars_weight = 0.378
 # WE USED FLOAT FOR CONVERTING THE DEFAULT STRING VALUE INTO FLOAT VALUE.
@@ -95,9 +97,83 @@ weight_on_Mars = round(weight_on_Earth * Mars_weight , 2)
 # F ALLOW US TO USE EXPRESSIONS INSIDE THE STRING.
 print(f'The equivalent weight on Mars: {weight_on_Mars}')
 
+print('\n')
+print('\n')
+print('\n')
 
 
 
 
-#* MILESTONE 02 PROBLEM :
+#? MILESTONE 02 PROBLEM :
 
+# CALCULATE THE WEIGHT ON DIIFERENT PLANETS IN OUR SOLAR SYSTEM.
+# TO CALCULATE THAT, WE HAVE CONSTANT VALUES FOR EACH PLANET.
+# STORE ALL THE CONSTANT VALUES FOR EACH VARIABLE.
+# THE RESULT SHOULD BE ACCORDING TO THE USER PLANET CHOICE
+
+
+
+#* PSEUDO CODE :
+
+# - Mercury: 37.6%
+
+# - Venus: 88.9%
+
+# - Mars: 37.8%
+
+# - Jupiter: 236.0%
+
+# - Saturn: 108.1%
+
+# - Uranus: 81.5%
+
+# - Neptune: 114.0%
+
+# PLANETS = THIER CONSTANT VALUES
+# ENTER WEIGHT ON EARTH
+# ENTER A PLANET
+# IF PLANET == SPECIFIC PLANET:
+    #  ROUND (EARTH WEIGHT VALUE * SPECIFIC PLANET VALUE , 2 )
+
+#* CODE :
+
+# STORED VALUES IN EACH CONSTANT
+Mercury , Venus , Mars , Jupiter , Saturn , Uranus , Neptune = 0.376 , 0.889 , 0.378 , 2.36 , 1.081 , 0.815 , 1.14
+
+weight_on_Earth = float(input('Enter weight on Earth: '))
+
+planet = input('Enter a Planet: ')
+
+if(planet == 'Mercury'):
+    weight_on_Mercury = round(weight_on_Earth * Mercury , 2 )
+    print(f'The equivalent weight on Mercury: {weight_on_Mercury} ')
+
+elif(planet == 'Venus'):
+    weight_on_Venus = round(weight_on_Earth * Venus , 2 )
+    print(f'The equivalent weight on Venus: {weight_on_Venus} ')
+
+elif(planet == 'Mars'):
+    weight_on_Mars = round(weight_on_Earth * Mars , 2 )
+    print(f'The equivalent weight on Mars: {weight_on_Mars} ')
+
+
+elif(planet == 'Jupiter'):
+    weight_on_Jupiter = round(weight_on_Earth * Jupiter , 2 )
+    print(f'The equivalent weight on Jupiter: {weight_on_Jupiter} ')
+
+
+elif(planet == 'Saturn'):
+    weight_on_Saturn = round(weight_on_Earth * Saturn , 2 )
+    print(f'The equivalent weight on Saturn: {weight_on_Saturn} ')
+
+
+elif(planet == 'Uranus'):
+    weight_on_Uranus = round(weight_on_Earth * Uranus , 2 )
+    print(f'The equivalent weight on Uranus: {weight_on_Uranus} ')
+
+
+elif(planet == 'Neptune'):
+    weight_on_Neptune = round(weight_on_Earth * Neptune , 2 )
+    print(f'The equivalent weight on Neptune: {weight_on_Neptune} ')
+else:
+    print('Invalid Input')
